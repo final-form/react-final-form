619dc7e23ae8045c7dbf32f133b889fd4b9fcf65
Erik Rasmussen <rasmussenerik@gmail.com>
2018-01-12 12:14:25 +0100

    v3.0.0

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@
 export { default as Field } from './Field'
 export { default as Form } from './ReactFinalForm'
 export { default as FormSpy } from './FormSpy'
-export const version = '2.2.2'
+export const version = '3.0.0'
