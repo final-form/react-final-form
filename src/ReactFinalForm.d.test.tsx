8e6596574269b6d14f4a13cb9c383db71c66a327
Kevin Ross <kevin.ross@alienfast.com>
2018-01-17 11:12:03 -0800

    remove unused comments

--- src/ReactFinalForm.d.test.tsx
+++ src/ReactFinalForm.d.test.tsx
@@ -63,37 +63,3 @@ const simpleSubscription = () => (
     )}
   </Form>
 )
-// const onSubmit: Config['onSubmit'] = (values, callback) => {}
-//
-// let form = createForm({ initialValues: {foo: 'bar'}, onSubmit })
-// let formState = form.getState()
-//
-// console.log(formState.active as string, formState.active as undefined)
-// console.log(formState.dirty as boolean)
-// console.log(formState.dirtySinceLastSubmit as boolean)
-// console.log(formState.error.foo, formState.error as string, formState.error as boolean)
-// console.log(formState.errors as AnyObject, formState.errors.foo)
-// console.log(formState.initialValues as AnyObject, formState.initialValues.foo)
-// console.log(formState.invalid as boolean)
-// console.log(formState.pristine as boolean)
-// console.log(formState.submitError as string, formState.submitError as object, formState.submitError as undefined)
-// console.log(formState.submitErrors as AnyObject, formState.submitErrors.foo)
-// console.log(formState.submitFailed as boolean)
-// console.log(formState.submitSucceeded as boolean)
-// console.log(formState.submitSucceeded as boolean)
-// console.log(formState.submitting as boolean)
-// console.log(formState.valid as  boolean)
-// console.log(formState.validating as boolean)
-// console.log(formState.values as AnyObject, formState.values.foo)
-//
-// const initialValues: Config['initialValues'] = {
-//   a: 'a',
-//   b: true,
-//   c: 1
-// }
-//
-// form = createForm({ onSubmit, initialValues })
-// formState = form.getState()
-//
-// console.log(formState.pristine as boolean)
-// console.log(formState.dirty as boolean)
