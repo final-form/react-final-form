802eb474a82004deadf544a65409c127c048b282
Erik Rasmussen <rasmussenerik@gmail.com>
2018-07-26 19:06:28 +0200

    Copied postinstall script from styled-components

---
+++ scripts/postinstall.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+/* eslint-disable */
+/* Adapted from nodemon's postinstall: https://github.com/remy/nodemon/blob/master/package.json */
+
+var msg =
+  'Use react-final-form at work? Consider supporting our development efforts at opencollective.com/final-form'
+
+console.log(msg)
