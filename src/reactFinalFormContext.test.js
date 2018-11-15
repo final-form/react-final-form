f14a42b9fdac41ff5f8bc716686e9c95a4600fdf
Gustavo Bastos <me@gustavovinicius.com>
2018-11-15 21:02:50 +0100

    Fix displayName of context provider HOC

--- src/reactFinalFormContext.test.js
+++ src/reactFinalFormContext.test.js
@@ -2,9 +2,27 @@ import React from 'react'
 import TestUtils from 'react-dom/test-utils'
 
 import Form from './ReactFinalForm'
-import { withReactFinalForm } from './reactFinalFormContext'
+import { getDisplayName, withReactFinalForm } from './reactFinalFormContext'
 
 describe('reactFinalFormContext', () => {
+  describe('getDisplayName', () => {
+    it('return displayName for Wrapped component with displayName', () => {
+      const Component = { displayName: 'Field' }
+
+      expect(getDisplayName(Component)).toEqual('ReactFinalForm(Field)')
+    })
+
+    it('return displayName for Wrapped component with name', () => {
+      const Component = { name: 'Field' }
+
+      expect(getDisplayName(Component)).toEqual('ReactFinalForm(Field)')
+    })
+
+    it('return FinalForm(Component) by default', () => {
+      expect(getDisplayName({})).toEqual('ReactFinalForm(Component)')
+    })
+  })
+
   it('should pass formApi using HOC', () => {
     const mockComponent = jest.fn(() => <div />)
     const render = () => {
