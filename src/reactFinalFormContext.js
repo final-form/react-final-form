a79792ae0b20b2617223d28eaed15f8c075d4da9
Gustavo Bastos <me@gustavovinicius.com>
2019-02-25 18:12:44 +0100

    Fix displayName of context provider HOC (#375)

--- src/reactFinalFormContext.js
+++ src/reactFinalFormContext.js
@@ -2,8 +2,16 @@ import * as React from 'react'
 
 export const ReactFinalFormContext = React.createContext(null)
 
+export const getDisplayName = Component => {
+  const displayName = Component.displayName || Component.name || 'Component'
+
+  return `ReactFinalForm(${displayName})`
+}
+
 export const withReactFinalForm = Component => {
   return class extends React.Component {
+    static displayName = getDisplayName(Component)
+
     render() {
       return React.createElement(ReactFinalFormContext.Consumer, {
         children: reactFinalForm =>
