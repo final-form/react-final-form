f349c07df75f67935e7c299d5924f37446b44577
Erik Rasmussen <rasmussenerik@gmail.com>
2019-06-04 19:58:36 +0200

    Remove context export (#515)
    * Added link to hooks-based error component example
    
    * doctoc readme
    
    * Removed context export

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 export { default as Field } from './Field'
 export { default as Form, version } from './ReactFinalForm'
 export { default as FormSpy } from './FormSpy'
-export { default as ReactFinalFormContext } from './context'
 export { default as useField } from './useField'
 export { default as useFormState } from './useFormState'
 export { default as useForm } from './useForm'
