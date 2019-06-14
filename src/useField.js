817c7311828141932a102c91e0198c9b26da9bde
Michal Tikotzky <michalt@qualitybath.com>
2019-06-14 15:42:27 -0400

    Use updated value for `formatOnBlur` format func (#465)
    * Use updated value for `formatOnBlur` format func
    
    * add test
    
    * made test more realistic (I think)

--- src/useField.js
+++ src/useField.js
@@ -112,7 +112,10 @@ function useField<FormValues: FormValuesShape>(
       (event: ?SyntheticFocusEvent<*>) => {
         state.blur()
         if (formatOnBlur) {
-          state.change(format(state.value, state.name))
+          const fieldState = form.getFieldState(state.name)
+          state.change(
+            format(fieldState ? fieldState.value : state.value, state.name)
+          )
         }
       },
       // eslint-disable-next-line react-hooks/exhaustive-deps
