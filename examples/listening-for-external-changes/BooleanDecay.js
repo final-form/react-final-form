84000f1e1fcb11a5df7263150d161de72efe5272
Uche Ozoemena <ucheozoemena@gmail.com>
2019-08-30 12:10:51 +0100

    Add code for listening for external changes

---
+++ examples/listening-for-external-changes/BooleanDecay.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+/**
+ * 👋 Hey! Thanks for being curious about what this component does!
+ * It's pretty simple. Have you ever seen those boxes where, when you
+ * flip the switch, it activates a motor that causes a little hand to
+ * come out and flip the switch back off? THAT'S what this component does.
+ *
+ * Examples: https://giphy.com/search/useless-box
+ *
+ * The value prop will be flipped back to false after the delay.
+ */
+export default class BooleanDecay extends React.Component {
+  static propTypes = {
+    children: PropTypes.func.isRequired,
+    delay: PropTypes.number.isRequired,
+    value: PropTypes.bool.isRequired
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: props.value
+    }
+  }
+
+  startTimer() {
+    this.stopTimer()
+    this.timeout = setTimeout(() => {
+      this.setState({ value: false })
+    }, this.props.delay)
+  }
+
+  stopTimer() {
+    if (this.timeout) {
+      clearTimeout(this.timeout)
+    }
+  }
+
+  componentDidMount() {
+    if (this.state.value) {
+      this.startTimer()
+    }
+  }
+
+  componentWillUnmount() {
+    this.stopTimer()
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { value } = nextProps
+    if (value !== this.state.value) {
+      this.setState({ value })
+      if (value) {
+        this.startTimer()
+      } else {
+        this.stopTimer()
+      }
+    }
+  }
+
+  render() {
+    return this.props.children(this.state.value)
+  }
+}
---
+++ examples/listening-for-external-changes/ExternalModificationDetector.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Field } from 'react-final-form'
+
+/**
+ * Listens for changes to a field's value, and, if the value changes
+ * when the field is NOT active, it sets an externallyModified flag
+ * which is passed to the children render function.
+ */
+class ExternalModificationDetector extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      externallyModified: false,
+      value: props.input.value
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { input: { value }, meta: { active } } = nextProps
+    if (value !== this.state.value) {
+      this.setState({
+        value,
+        externallyModified: !active
+      })
+    } else if (this.state.externallyModified) {
+      this.setState({
+        externallyModified: false
+      })
+    }
+  }
+
+  render() {
+    const { children } = this.props
+    const { externallyModified } = this.state
+    return children(externallyModified)
+  }
+}
+
+// Wrap the ExternalModificationDetector in a Field
+export default ({ name, children }) => (
+  <Field
+    name={name}
+    subscription={{ value: true, active: true }}
+    render={props => (
+      <ExternalModificationDetector {...props}>
+        {children}
+      </ExternalModificationDetector>
+    )}
+  />
+)
