916db006f8f08ad2bd5e164735b724fff78482aa
Erik Rasmussen <rasmussenerik@gmail.com>
2021-07-23 14:51:46 +0200

    Use custom hook to create callback constants (#933)
    * Use custom hook to create callback constants
    
    * Switched to @mpenney99's solution

---
+++ src/useConstantCallback.js
@@ -0,0 +1,16 @@
+// @flow
+import * as React from 'react'
+
+/**
+ * Creates a callback, even with closures, that will be
+ * instance === for the lifetime of the component, always
+ * calling the most recent version of the function and its
+ * closures.
+ */
+export default function useConstantCallback(callback) {
+  const ref = React.useRef(callback)
+  React.useEffect(() => {
+    ref.current = callback
+  })
+  return React.useCallback((...args) => ref.current.apply(null, args), [])
+}
