2ad83082f6d3281051337678146b09905d9ab8f2
Erik Rasmussen <rasmussenerik@gmail.com>
2021-09-29 12:21:04 +0200

    FF 4.20.3 and dep upgrades

--- .babelrc.js
+++ .babelrc.js
@@ -18,18 +18,6 @@ module.exports = {
     "@babel/plugin-transform-flow-strip-types",
     "@babel/plugin-syntax-dynamic-import",
     "@babel/plugin-syntax-import-meta",
-    ["@babel/plugin-proposal-class-properties", { loose }],
-    "@babel/plugin-proposal-json-strings",
-    [
-      "@babel/plugin-proposal-decorators",
-      {
-        legacy: true,
-      },
-    ],
-    "@babel/plugin-proposal-function-sent",
-    "@babel/plugin-proposal-export-namespace-from",
-    "@babel/plugin-proposal-numeric-separator",
-    "@babel/plugin-proposal-throw-expressions",
     test && "@babel/plugin-transform-react-jsx-source",
   ].filter(Boolean),
 };
