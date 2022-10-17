c77981720baf90b4504af3a8f87fc0b14175423c
Becker63 <johnsontaylor6320@gmail.com>
2022-10-17 14:13:14 -0400

    initial commit

--- examples/chakra/index.js
+++ examples/chakra/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
-import React from 'react'
-import { render } from 'react-dom'
+import React from "react";
+import { render } from "react-dom";
 import {
   Box,
   Button,
@@ -21,16 +21,16 @@ import {
   RadioGroup,
   Stack,
   Textarea
-} from '@chakra-ui/core'
-import { Form, Field, useField, useForm } from 'react-final-form'
-import validate from './validate'
+} from "@chakra-ui/core";
+import { Form, Field, useField, useForm } from "react-final-form";
+import validate from "./validate";
 
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const onSubmit = async values => {
-  await sleep(300)
-  window.alert(JSON.stringify(values, 0, 2))
-}
+const onSubmit = async (values) => {
+  await sleep(300);
+  window.alert(JSON.stringify(values, 0, 2));
+};
 
 const App = () => (
   <ThemeProvider theme={theme}>
@@ -43,7 +43,7 @@ const App = () => (
         Chakra Example
       </Heading>
       <Box as="p" textAlign="center">
-        Example using React Final Form and{' '}
+        Example using React Final Form and{" "}
         <Link href="https://chakra-ui.com" isExternal>
           Chakra <Icon name="external-link" mx="2px" />
         </Link>
@@ -80,48 +80,7 @@ const App = () => (
             */}
             <InputControl name="firstName" label="First Name" />
             <InputControl name="lastName" label="Last Name" />
-            <CheckboxControl name="employed">Employed</CheckboxControl>
-            <Field
-              name="favoriteColor"
-              component={AdaptedRadioGroup}
-              label="Favorite Color"
-            >
-              <Radio value="#ff0000" color="red">
-                Red
-              </Radio>
-              <Radio value="#00ff00" color="green">
-                Green
-              </Radio>
-              <Radio value="#0000ff" color="blue">
-                Blue
-              </Radio>
-            </Field>
-            <Control name="toppings" my={4}>
-              <FormLabel htmlFor="toppings">Toppings</FormLabel>
-              <Stack pl={6} mt={1} spacing={1}>
-                <CheckboxArrayControl name="toppings" value="chicken">
-                  🐓 Chicken
-                </CheckboxArrayControl>
-                <CheckboxArrayControl name="toppings" value="ham">
-                  🐷 Ham
-                </CheckboxArrayControl>
-                <CheckboxArrayControl name="toppings" value="mushrooms">
-                  🍄 Mushrooms
-                </CheckboxArrayControl>
-                <CheckboxArrayControl name="toppings" value="cheese">
-                  🧀 Cheese
-                </CheckboxArrayControl>
-                <CheckboxArrayControl name="toppings" value="tuna">
-                  🐟 Tuna
-                </CheckboxArrayControl>
-                <CheckboxArrayControl name="toppings" value="pineapple">
-                  🍍 Pineapple
-                </CheckboxArrayControl>
-              </Stack>
-              <Error name="toppings" />
-            </Control>
-            <TextareaControl name="notes" label="Notes" />
-            <PercentComplete size="sm" my={5} hasStripe isAnimated />
+
             <ButtonGroup spacing={4}>
               <Button
                 isLoading={submitting}
@@ -148,19 +107,19 @@ const App = () => (
       />
     </Box>
   </ThemeProvider>
-)
+);
 
 const AdaptedTextarea = ({ input, meta, ...rest }) => (
   <Textarea {...input} {...rest} isInvalid={meta.error && meta.touched} />
-)
+);
 
 const CheckboxControl = ({ name, value, children }) => {
   const {
     input: { checked, ...input },
     meta: { error, touched, invalid }
   } = useField(name, {
-    type: 'checkbox' // important for RFF to manage the checked prop
-  })
+    type: "checkbox" // important for RFF to manage the checked prop
+  });
   return (
     <FormControl isInvalid={touched && invalid} my={4}>
       <Checkbox {...input} isInvalid={touched && invalid} my={4}>
@@ -168,23 +127,23 @@ const CheckboxControl = ({ name, value, children }) => {
       </Checkbox>
       <FormErrorMessage>{error}</FormErrorMessage>
     </FormControl>
-  )
-}
+  );
+};
 
 const CheckboxArrayControl = ({ name, value, children }) => {
   const {
     input: { checked, ...input },
     meta: { error, touched }
   } = useField(name, {
-    type: 'checkbox', // important for RFF to manage the checked prop
+    type: "checkbox", // important for RFF to manage the checked prop
     value // important for RFF to manage list of strings
-  })
+  });
   return (
     <Checkbox {...input} isChecked={checked} isInvalid={error && touched}>
       {children}
     </Checkbox>
-  )
-}
+  );
+};
 
 const AdaptedRadioGroup = ({ input, meta, label, children }) => (
   <FormControl isInvalid={meta.touched && meta.invalid} my={4}>
@@ -192,24 +151,24 @@ const AdaptedRadioGroup = ({ input, meta, label, children }) => (
     <RadioGroup {...input}>{children}</RadioGroup>
     <FormErrorMessage>{meta.error}</FormErrorMessage>
   </FormControl>
-)
+);
 
 const Control = ({ name, ...rest }) => {
   const {
     meta: { error, touched }
-  } = useField(name, { subscription: { touched: true, error: true } })
-  return <FormControl {...rest} isInvalid={error && touched} />
-}
+  } = useField(name, { subscription: { touched: true, error: true } });
+  return <FormControl {...rest} isInvalid={error && touched} />;
+};
 
 const Error = ({ name }) => {
   const {
     meta: { error }
-  } = useField(name, { subscription: { error: true } })
-  return <FormErrorMessage>{error}</FormErrorMessage>
-}
+  } = useField(name, { subscription: { error: true } });
+  return <FormErrorMessage>{error}</FormErrorMessage>;
+};
 
 const InputControl = ({ name, label }) => {
-  const { input, meta } = useField(name)
+  const { input, meta } = useField(name);
   return (
     <Control name={name} my={4}>
       <FormLabel htmlFor={name}>{label}</FormLabel>
@@ -221,8 +180,8 @@ const InputControl = ({ name, label }) => {
       />
       <Error name={name} />
     </Control>
-  )
-}
+  );
+};
 
 const TextareaControl = ({ name, label }) => (
   <Control name={name} my={4}>
@@ -235,18 +194,18 @@ const TextareaControl = ({ name, label }) => (
     />
     <Error name={name} />
   </Control>
-)
+);
 
-const PercentComplete = props => {
-  const form = useForm()
-  const numFields = form.getRegisteredFields().length
-  const numErrors = Object.keys(form.getState().errors).length
+const PercentComplete = (props) => {
+  const form = useForm();
+  const numFields = form.getRegisteredFields().length;
+  const numErrors = Object.keys(form.getState().errors).length;
   return (
     <Progress
       value={numFields === 0 ? 0 : ((numFields - numErrors) / numFields) * 100}
       {...props}
     />
-  )
-}
+  );
+};
 
-render(<App />, document.getElementById('root'))
+render(<App />, document.getElementById("root"));
